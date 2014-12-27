959b3c6e3fb58a7335b11df084cbe92e51250864
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2014-12-27 19:12:21 +0100

    Poprawiony maly bug

--- R/chooseClassifier.R
+++ R/chooseClassifier.R
@@ -6,26 +6,21 @@
 #' 
 #' For an object of class \code{formula} and a data set the 
 #' \code{chooseClassifier} computes a \link{ggplot} showing
-#' the Area Under Curve (result of \link[performance]{ROCR}) for
+#' the Area Under the Curve (result of \link[ROCR]{performance}) for
 #' classification algorithms like \link{naiveBayes}, \link{lda}, 
-#' logistic regression \link{glm} with \code{logit} link function.
+#' logistic regression \link{glm} with \code{logit} or \code{probit} link function.
 #' Any classifier can be added if needed. Contact: 
 #' \href{https://github.com/sandersone/twd1/issues}{https://github.com/sandersone/twd1/issues}.
 #' 
 #' @note
-#' For k-nearest neighbourhood alghoritm, the model with the highest Area
-#' Under Curve is shown for \code{k} from 2 to 6. So far it is commented
-#' because I have problems with that function.
 #' 
 #' For naive Bayes a parameter \code{laplace} is set to 0.2.
 #' 
 #'
-#' @param formula \code{formula}
-#' @param train \code{data.frame}
-#' @param train \code{data.frame}
+#' @param formula A \code{formula} for classifiers to compute.
+#' @param train A \code{data.frame} denoting the training set.
+#' @param test A \code{data.frame} denoting the test set.
 #' 
-#' @details
-#' TO DO
 #'
 #'
 #' @examples
@@ -74,9 +69,16 @@ chooseClassifier <- function( formula, train, test ){
    P <- predict( logit, newdata = test, type="response")
    Pred  <-  ifelse( P >0.5, 1, 0 )
    auces$logit_auc <- auc( Pred, test[, as.character( formula )[2]] )
+
+
+   # probit
+   probit  <-  glm( formula, data = train, family=binomial(link = "probit"))
+   P <- predict( probit, newdata = test, type="response")
+   Pred  <-  ifelse( P >0.5, 1, 0 )
+   auces$probit_auc <- auc( Pred, test[, as.character( formula )[2]] )
    
    
-   plotClassifiers( auces )
+   plotClassifiers( as.list(auces) )
 
 }
 
@@ -90,18 +92,24 @@ auc <- function(predicted_probability, real_classes){
 
 plotClassifiers <- function( list ){
    assert_that( is.list( list ) )
-   n <- length(auces)
+   n <- length(list)
    
-   auces <- as.data.frame(t(as.data.frame(list)))
-   auces[, 2] <- rownames(auces)
+   aucesX <- as.data.frame(t(as.data.frame(list)))
+   aucesX[, 2] <- rownames(aucesX)
 #    auces[, 3] <- rep(1, n)
 #    names(auces) <- c("auc", "algorithm", "fake")
-   names(auces) <- c("auc", "algorithm")
-   ggplot(auces, aes(x= factor(algorithm), y=auc, fill= auc))+ 
+   names(aucesX) <- c("auc", "algorithm")
+   ggplot(aucesX, aes(x= factor(algorithm), y=auc, fill= auc))+ 
       geom_bar(stat = "identity")+
          scale_fill_gradient(low="#BA55D3", high="#8968CD")+
       xlab("Algorithms")+
-      ylab("Area Undr Curve")
+      ylab("Area Under the Curve")+
+   theme( axis.text.x = element_text(family = "mono", size=15),
+          axis.title.x= element_text(family = "mono", size=15),
+          axis.title.y= element_text(family = "mono", size=15),
+          title =element_text(family = "mono", size = 18)#, legend.position = "top"
+   ) +ggtitle("Comparison of the \n classification algorithms")+
+   scale_x_discrete(labels=c("naiveBayes","LDA","logistic \n regression","probit \n regression"))
    
    
    
