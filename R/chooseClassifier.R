6e614a6caf7047ea3b29847b67fd0396b9fd4894
sandersone <sandersone@student.mini.pw.edu.pl>
2015-01-06 11:41:04 +0100

    Dodanie pozostałych klasyfikatorów. Trzeba poprawić wielkość czcionki, sprawdzic poprawność AUC, przygotować 2-3 przykłady, najlepiej gotowe w jakijś formie żeby nei trzeba było tego odpalać na zajęciach (długo się robi).

--- R/chooseClassifier.R
+++ R/chooseClassifier.R
@@ -36,11 +36,11 @@
 #' @family classTools
 #' @rdname chooseClassifier
 #' @export
-chooseClassifier <- function( formula, train, test ){
-   assert_that( is.data.frame( train ), is.data.frame( test ) )
+chooseClassifier <- function( formula, train, test, choice = c(1,1,1,0,0,0,0,0) ){
+   assert_that( is.data.frame( train ), is.data.frame( test ),length(choice)==8 )
    formula <- as.formula( formula )
-   auces <- list() 
-   preces <- list()
+#   auces <- list() 
+#  preces <- list()
 
 #    choosing best k for knn
 #    knn_auc <- sapply( 2:6, function( x ){
@@ -55,8 +55,8 @@ chooseClassifier <- function( formula, train, test ){
    bayes_pred <- predict( bayes, newdata = test )
    bayes_prawd <- predict( bayes, newdata = test, type="raw")[,2]
    
-   auces$bayes_auc <- auc( bayes_prawd, test[, as.character( formula )[2]] )
-   preces$bayes_prec <- sum(diag(table(bayes_pred,test$class)))/sum((table(bayes_pred,test$class)))
+#   auces$bayes_auc <- auc( bayes_prawd, test[, as.character( formula )[2]] )
+#   preces$bayes_prec <- sum(diag(table(bayes_pred,test$class)))/sum((table(bayes_pred,test$class)))
 
    auc_prec <- data.frame(AUC=auc( bayes_prawd, test[, as.character( formula )[2]] ),
                            PREC=sum(diag(table(bayes_pred,test$class)))/sum((table(bayes_pred,test$class))),
@@ -68,8 +68,8 @@ chooseClassifier <- function( formula, train, test ){
    pred_klas <- predict( mod_lda, newdata = test )$class
    pred_praw <- predict( mod_lda, newdata = test )$posterior[,2]
    
-   auces$lda_auc <- auc( pred_praw, test[, as.character( formula )[2]] )
-   preces$lda_prec <- sum(diag(table(pred_klas,test$class)))/sum((table(pred_klas,test$class)))
+#   auces$lda_auc <- auc( pred_praw, test[, as.character( formula )[2]] )
+#   preces$lda_prec <- sum(diag(table(pred_klas,test$class)))/sum((table(pred_klas,test$class)))
    
    auc_prec <- rbind(auc_prec,data.frame(AUC=auc( pred_praw, test[, as.character( formula )[2]] ),
                                  PREC=sum(diag(table(pred_klas,test$class)))/sum((table(pred_klas,test$class))),
@@ -79,19 +79,19 @@ chooseClassifier <- function( formula, train, test ){
    logit  <-  glm( formula, data = train, family=binomial(link = "logit"))
    P <- predict( logit, newdata = test, type="response")
    Pred  <-  ifelse( P >0.5, 1, 0 )
-   auces$logit_auc <- auc( Pred, test[, as.character( formula )[2]] )
-   preces$logit_prec <- sum(diag(table(P,test$class)))/sum((table(P,test$class)))
+#   auces$logit_auc <- auc( Pred, test[, as.character( formula )[2]] )
+#   preces$logit_prec <- sum(diag(table(P,test$class)))/sum((table(P,test$class)))
 
    auc_prec <- rbind(auc_prec,data.frame(AUC= auc( Pred, test[, as.character( formula )[2]] ),
-                  PREC=sum(diag(table(P,test$class)))/sum((table(P,test$class))),
+                  PREC=sum(diag(table(Pred,test$class)))/sum((table(P,test$class))),
                   CLASSIFIER="Logit"))
 
    # probit
    probit  <-  glm( formula, data = train, family=binomial(link = "probit"))
    P <- predict( probit, newdata = test, type="response")
    Pred  <-  ifelse( P >0.5, 1, 0 )
-   auces$probit_auc <- auc( Pred, test[, as.character( formula )[2]] )
-   preces$probit_prec <- sum(diag(table(Pred,test$class)))/sum((table(Pred,test$class)))
+#   auces$probit_auc <- auc( Pred, test[, as.character( formula )[2]] )
+#   preces$probit_prec <- sum(diag(table(Pred,test$class)))/sum((table(Pred,test$class)))
    
    auc_prec <- rbind(auc_prec,data.frame(AUC= auc( Pred, test[, as.character( formula )[2]] ),
                                 PREC=sum(diag(table(Pred,test$class)))/sum((table(Pred,test$class))),
@@ -104,7 +104,37 @@ chooseClassifier <- function( formula, train, test ){
    auc_prec <- rbind(auc_prec,data.frame(AUC= auc( as.numeric(SVM_pred)-1, test[, as.character( formula )[2]] ),
                                       PREC=sum(diag(table(SVM_pred,test$class)))/sum((table(SVM_pred,test$class))),
                                       CLASSIFIER="SVM"))
+
+   # drzewo (trzeba do depedencies dopisać library(rpart) )
+
+   drzewo <- rpart(formula,data=train)
+   drzewo_pred <- predict(drzewo,newdata=test,type="class")
+
+   auc_prec <- rbind(auc_prec,data.frame(AUC= auc( as.numeric(drzewo_pred)-1, test[, as.character( formula )[2]] ),
+                                      PREC=sum(diag(table(drzewo_pred,test$class)))/sum((table(drzewo_pred,test$class))),
+                                      CLASSIFIER="Tree"))
+
+   # boosting (trzeba dopisać adabag)
    
+   boost <- boosting(formula, data = train)
+   boost_pred <- predict(boost,test)$class
+
+   auc_prec <- rbind(auc_prec,data.frame(AUC= auc( as.numeric(boost_pred)-1, test[, as.character( formula )[2]] ),
+                                      PREC=sum(diag(table(boost_pred,test$class)))/sum((table(boost_pred,test$class))),
+                                      CLASSIFIER="Boost"))
+
+   # bagging (trzeba dopisać adabag)
+
+   bag <- bagging(formula, data = train)
+   bag_pred <- predict(bag,test)$class
+   
+   auc_prec <- rbind(auc_prec,data.frame(AUC= auc( as.numeric(bag_pred)-1, test[, as.character( formula )[2]] ),
+                                         PREC=sum(diag(table(bag_pred,test$class)))/sum((table(bag_pred,test$class))),
+                                         CLASSIFIER="Bag"))
+
+
+   auc_prec <- auc_prec[as.logical(choice),]
+
    plotClassifiers( data.frame(auc_prec) )
 
 }
@@ -133,17 +163,17 @@ plotClassifiers <- function( list ){
    ggplot(aucesX, aes(x= factor(algorithm), y=auc, fill= auc))+ 
       geom_bar(stat = "identity")+
       geom_point(aes(x=factor(algorithm),y=prec))+
-      geom_text(aes(x=factor(algorithm),y=prec,label=prec))+
+      geom_text(aes(x=factor(algorithm),y=prec,label=paste0(round(prec,3)*100,"%"),size=prec),vjust=1.8)+
+   # tu trzeba zrobić coś, żeby ten najmniejszy napis nie był taki maleńki...
          scale_fill_gradient(low="#BA55D3", high="#8968CD")+
       xlab("Algorithms")+
-      ylab("Area Under the Curve")+
+      ylab("Area Under the Curve / Precision")+
    theme( axis.text.x = element_text(family = "mono", size=15),
           axis.title.x= element_text(family = "mono", size=15),
           axis.title.y= element_text(family = "mono", size=15),
           title =element_text(family = "mono", size = 18)#, legend.position = "top"
-   ) +ggtitle("Comparison of the \n classification algorithms")+
-   scale_x_discrete(labels=c("naiveBayes","LDA","logistic \n regression","probit \n regression"))
+   ) +ggtitle("Comparison of chosen \n classification algorithms")+
+   scale_x_discrete(labels=aucesX[,3])
    
-   
-   
+
 }
